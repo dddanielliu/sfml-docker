9689f1a4d201859583e154b833fe9c072f3111a7
dddanielliu <dddanielliu@gmail.com>
2025-02-27 11:57:23 +0800

    update start script

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 image_name="sfml"
 port=3000
 
-docker ps -q --filter "ancestor=$image_name" | xargs -I {} docker stop {}
+docker ps -q --filter "ancestor=$image_name" | xargs -I {} docker stop {} | xargs -I {} echo "Stopped {}"
 
 if ! [ -z "$(docker images -q $image_name) 2> /dev/null)" ]; then
     docker build -t $image_name .;
