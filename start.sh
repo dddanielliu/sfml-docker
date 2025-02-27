bf62e713827ba107c6c76d728d7317ac791da903
dddanielliu <dddanielliu@gmail.com>
2025-02-27 11:42:21 +0800

    add start script

---
+++ start.sh
@@ -0,0 +1,11 @@
+image_name="sfml"
+port=3000
+
+docker ps -q --filter "ancestor=$image_name" | xargs -I {} docker stop {}
+
+if ! [ -z "$(docker images -q $image_name) 2> /dev/null)" ]; then
+    docker build -t $image_name .;
+fi
+
+
+docker run --rm -itd -p $port:3000 $image_name bash
